ddae64f980bab8e1628128def120853938016a8e
Damien De Campos <damiensporting@gmail.com>
2023-04-13 21:45:32 +0200

    Adding conversation

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Tests
-xmllint test/kernel/answer1.xml --schema schemas/kernel/core/message.xsd --noout
-xmllint test/kernel/question1.xml --schema schemas/kernel/core/message.xsd --noout
+xmllint test/kernel/answer1.xml --schema schemas/kernel/message.xsd --noout
+xmllint test/kernel/question1.xml --schema schemas/kernel/conversation.xsd --noout
